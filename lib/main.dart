442461ff52f610807890a1b6e22fc5b82d8bcb8c
Zinu Bernardo <zinubernardo@gmail.com>
2026-02-26 15:21:28 +0100

    feat: integra Firebase Core e configura inicialização no Android

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:missao_abc/features/auth/screens/profile_selection_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MissaoABCApp(),
